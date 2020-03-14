3ab36e6c14e91ecaf3d2726aa3146d07137bfb57
lesley <lesleybanadzem2017@gmail.com>
2020-03-14 12:28:48 -0700

    Wrote test to test findByUserId

--- auth/auth-model.spec.js
+++ auth/auth-model.spec.js
@@ -1,18 +1,28 @@
 const bcrypt = require("bcryptjs");
 const db = require("../database/db-config");
-
-beforeEach(()=>{
-    db.seed.run();
+const authModels = require('./auth-model');
+beforeEach(async ()=>{
+    await db.seed.run();
 })
 
-describe('Find user by id',  () => {
+describe('Find user',  () => {
     test('Query all users in the db', async () => {
-        
+
         const users = await db('users');
 
         expect(users).toHaveLength(3);
         expect(users).not.toBeUndefined()
         expect(users[0].first_name).toBe('dang')
     })
-    
+})
+describe('Find user by id',  () => {
+    test('Query based on the user id', async () => {
+
+        const user = await authModels.findUsersById(2).first();
+
+        expect(user).not.toBeUndefined()
+        expect(user.id).toEqual(2)
+        expect(typeof user).toBe('object');
+        expect(user).toMatchObject({first_name: 'daniel', last_name: 'martin', email: 'daniel@carpal.com'})
+    })
 })
