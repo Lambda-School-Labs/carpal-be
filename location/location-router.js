25ed95508e28e66c531c4b62f9d86f100a1b7b35
dang lu <danglu@dangs-mbp.lan>
2020-03-16 18:49:23 -0700

    added endpoint for favorite location

--- location/location-router.js
+++ location/location-router.js
@@ -1,9 +1,29 @@
 const { Models } = require("../ModelClass/Models");
 const express = require("express");
+const db = require('../database/db-config')
 
 const router = express.Router();
 
+const { verifyToken, validateUserToken } = require('../Middleware/auth')
 const locations = new Models("locations");
+
+class FavLocations extends Models {
+    constructor(name) {
+        super(name)
+        this.name = 'favorite_locations'
+    }
+
+    getFavorites(user_id) {
+        return db(`${this.name} as f`)
+            .join(`locations as l`, 'l.id', 'f.location_id')
+            .join('users as u', 'u.id', 'f.user_id')
+            .where({ 'u.id': user_id })
+            .select('u.id', 'l.name', 'l.address', 'l.city', 'l.state', 'l.zip_code')
+    }
+}
+
+const FaveLocations = new FavLocations()
+
 
 router.get("/", async (req, res, next) => {
     try {
@@ -12,6 +32,16 @@ router.get("/", async (req, res, next) => {
         next(err);
     }
 });
+
+router.get('/favorites', verifyToken(), validateUserToken(), async (req, res, next) => {
+    try {
+
+        res.json(await FaveLocations.getFavorites(req.user.id))
+    }
+    catch (err) {
+        next(err)
+    }
+})
 
 //needs middleware validation for valid ID
 router.get("/:id", async (req, res, next) => {
