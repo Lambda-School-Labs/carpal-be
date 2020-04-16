abb05c1d5bc3c68b38d21040c3c1b014b0eaa48c
Ruth Poliakon <r.poliakon@gmail.com>
2020-04-15 20:00:29 -0600

    riders table creation

--- database/migrations/20200415174944_rides.js
+++ database/migrations/20200415174944_rides.js
@@ -1,38 +1,48 @@
+exports.up = async function (knex) {
+    await knex.schema.createTable("rides", (tbl) => {
+        tbl.increments();
+        tbl.integer("driver_id")
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.integer("rider_id")
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.integer("start_location_id")
+            .notNullable()
+            .references("id")
+            .inTable("locations")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.integer("end_location_id")
+            .notNullable()
+            .references("id")
+            .inTable("locations")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.string("status").defaultTo("pending");
+    });
 
-exports.up = async function(knex) {
-  await knex.schema.createTable("rides", (tbl) => {
-    tbl.increments();
-    tbl.integer("driver_id")
-        .notNullable()
-        .references("id")
-        .inTable("users")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE")
-    tbl.integer("rider_id")
-        .notNullable()
-        .references("id")
-        .inTable("users")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE")
-    tbl.integer("start_location_id")
-        .notNullable()
-        .references("id")
-        .inTable("locations")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE")
-    tbl.integer("end_location_id")
-        .notNullable()
-        .references("id")
-        .inTable("locations")
-        .onUpdate("CASCADE")
-        .onDelete("CASCADE")
-    tbl.string("status")
-        .defaultTo("pending")
-  });
-
-  
+    await knex.schema.createTable("riders", (tbl) => {
+        tbl.integer("ride_id")
+            .notNullable()
+            .references("id")
+            .inTable("rides")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.integer("user_id")
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        tbl.primary(["user_id", "ride_id"]);
+    });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
