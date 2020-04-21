f3bc2980d89cb71a9c0668ae6e05970c844a3f46
Darren <dangus1924@gmail.com>
2020-04-20 21:54:03 -0400

    pull ruth new change

---
+++ rides/rides-router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const { Rides } = require("../Classes/rides");
+
+const rides = new Rides();
+
+router.post("/", async (req, res, next) => {
+    try {
+        const requestBody = {
+            rider_id: req.body.rider_id,
+            start_location_id: req.body.start_location_id,
+            end_location_id: req.body.end_location_id
+        };
+    }
+});
+
+module.exports = router;
