520ee9e6cb12778d4e38a8f9be7e7ca203fbbe87
Ruth Poliakon <r.poliakon@gmail.com>
2020-04-22 18:19:25 -0600

    rides-post endpoint created, and tested

--- rides/__tests__/rides-post.spec.js
+++ rides/__tests__/rides-post.spec.js
@@ -1,11 +1,30 @@
-// const supertest = require("supertest");
-// const server = require("../index");
-// const db = require("../database/db-config");
-// let ride;
+const supertest = require("supertest");
+const server = require("../../index");
+const db = require("../../database/db-config");
 
-// beforeAll(async () => {
-//     await db.seed.run();
-//     rides = await (await supertest(server).post("/rides")).setEncoding
-//         start_location
-//     });
-// });
+let user;
+
+beforeAll(async () => {
+    await db.seed.run();
+    //login a user to grab token
+    user = await supertest(server)
+        .post("/auth/login")
+        .send({ email: "dang@carpal.com", password: "abc123" });
+});
+
+describe("Rides Router", () => {
+    test("Add new saved ride", async () => {
+        const res = await supertest(server)
+            .post("/users/rides")
+            .send({
+                start_location_id: 1,
+                end_location_id: 2
+            })
+            .set({ authorization: user.body.token });
+
+        expect(res.status).toBe(201);
+        expect(res.type).toEqual("application/json");
+        expect(res.body).toHaveProperty("driver_id", 1);
+        expect(res.body.id).toBeGreaterThan(0);
+    });
+});
