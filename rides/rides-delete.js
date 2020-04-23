6533d77b6a8e6f83f972e6aff92c81ac2b108dfc
Dennis Padiernos <depadiernos@gmail.com>
2020-04-22 20:23:51 -0500

    flesh out rides delete method

--- rides/rides-delete.js
+++ rides/rides-delete.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { Rides } = require("../Classes/rides");
+const { validateRideId } = require("../Middleware/auth")
 
 const rides = new Rides();
 
-router.delete("/", async (req, res, next) => {
+router.delete("/", validateRideId(), async (req, res, next) => {
     try {
+        const ride_id = req.body.ride_id;
+        res.json(await rides.delete(ride_id));
     } catch (err) {
         next(err);
     }
