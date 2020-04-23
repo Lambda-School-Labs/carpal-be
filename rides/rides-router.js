dc6e81b6d8259392386dd09e2dab6e81a9be965b
Darren <dangus1924@gmail.com>
2020-04-22 20:05:31 -0400

    created the first get for rides; get all rides

--- rides/rides-router.js
+++ rides/rides-router.js
@@ -5,27 +5,9 @@ const ridesGet = require("./rides-get");
 const ridesDelete = require("./rides-delete");
 const ridesPost = require("./rides-post");
 
-<<<<<<< HEAD
-const rides = new Rides();
-
-router.post("/", async (req, res, next) => {
-    try {
-        const requestBody = {            
-            start_location_id: req.body.start_location_id,
-            end_location_id: req.body.end_location_id,
-            driver_id: req.user.id
-        };
-
-        res.status(201).json(await rides.add(requestBody));
-    } catch (err) {
-        next(err);
-    }
-});
-=======
 router.use("/", ridesPut);
 router.use("/", ridesGet);
 router.use("/", ridesDelete);
 router.use("/", ridesPost);
->>>>>>> 7825201cf47c9b2502c2c53a2a85f40850d27c22
 
 module.exports = router;
