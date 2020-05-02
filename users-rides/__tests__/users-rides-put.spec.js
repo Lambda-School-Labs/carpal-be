f6854727284b85719be7d382704f3dd46bed22df
Dennis Padiernos <depadiernos@gmail.com>
2020-05-01 22:27:41 -0500

    take into account the new validateRideId() middleware looking for ride_id from the body.

--- users-rides/__tests__/users-rides-put.spec.js
+++ users-rides/__tests__/users-rides-put.spec.js
@@ -5,20 +5,22 @@ const db = require("../../database/db-config");
 beforeEach(async () => {
     await db.seed.run();
 });
-
-const token = global.token
+afterAll(async () => {
+    await db.destroy()
+})
 
 describe("Rides-PUT-Router", () => {
     it("returns 200 OK", async () => {
         const res = await supertest(server)
-            .put("/users/rides/1")
+            .put("/users/rides")
             .send({
+                ride_id: 1,
+                driver_id: 1,
                 start_location_id: 1,
                 end_location_id: 2,
                 status: "pending"
             })
-            .set({ authorization: token });
-
+            .set({ authorization: global.token });
         expect(res.status).toBe(200);
         expect(res.type).toEqual("application/json");
     });
--- users-rides/users-rides-put.js
+++ users-rides/users-rides-put.js
@@ -5,11 +5,11 @@ const { validateRideId } = require("../Middleware/auth");
 
 const rides = new Rides();
 
-router.put("/:id", validateRideId(), async (req, res, next) => {
-    const { id } = req.params;
-    const changes = req.body;
+router.put("/", validateRideId(), async (req, res, next) => {
+    const { driver_id, start_location_id, end_location_id, status } = req.body;
+    const changes = { driver_id, start_location_id, end_location_id, status };
     try {
-        const updatedRide = await rides.update(id, changes);
+        const updatedRide = await rides.update(req.ride.id, changes);
         res.status(200).json(updatedRide);
     } catch (err) {
         next(err);
