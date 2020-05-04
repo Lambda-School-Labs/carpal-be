bedb9df05a645846cac60120cfdac68c1428d2fd
Daniel Martin <danielmartin1205@gmail.com>
2020-05-04 19:07:42 -0400

    added twilio message on confirmed request put

--- requests/requests-put.js
+++ requests/requests-put.js
@@ -2,11 +2,23 @@ const express = require("express");
 const router = express.Router();
 const { Requests } = require("../Classes/requests");
 const requests = new Requests();
+const client = require("../config/twilio");
 
 router.put("/", async (req, res, next) => {
     try {
+        if (req.body.status === "confirmed") {
+            client.messages.create({
+                body: `Your ride has been confirmed`, // add ETA and driver details/name
+                from: process.env.TWILIO_FROM_PHONE,
+                to: req.user.phone_number
+            });
+        }
         res.status(200).json(
-            await requests.update(req.body.ride_id, req.body.rider_id, req.body.status)
+            await requests.update(
+                req.body.ride_id,
+                req.body.rider_id,
+                req.body.status
+            )
         );
     } catch (err) {
         next(err);
