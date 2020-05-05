45bf72ff7837cc1a0732d21977c046b181bc115a
smv5047 <smv5047@gmail.com>
2020-05-05 19:31:54 -0400

    added helmet back and allowed origins for cors Co-authored-by: Dennis Padiernos <depadiernos@gmail.com>

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ const app = express();
 
 const cors = require("cors");
 const helmet = require("helmet");
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://letscarpal.com",
+    "https://staging.d3ic1rxl46vguk.amplifyapp.com/"
+];
 
 //Middlewaress
 app.use(express.json());
-// app.use(helmet());
-app.use(cors());
+app.use(helmet());
+app.use(
+    cors({
+        origin: allowedOrigins
+    })
+);
 app.enable("trust proxy");
 
 app.get("/", function (req, res) {
