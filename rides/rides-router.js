aaae70d255b40564613d7cd53413c2bd25a4ce82
Daniel Martin <danielmartin1205@gmail.com>
2020-05-11 21:45:41 -0400

    changed rides route to accept a query instead of body, added user details to else statement

--- rides/rides-router.js
+++ rides/rides-router.js
@@ -14,29 +14,26 @@ const audio_dislikesDB = new UserDetails("audio", "users_audio_dislikes");
 router.get("/", async (req, res, next) => {
     try {
         const allRides = await rides.getDetail();
-        const { start_location, end_location } = req.body;
-        if (start_location && end_location) {
+        const { start_location, end_location } = req.query;
+
+        const start = JSON.parse(start_location);
+
+        const end = JSON.parse(end_location);
+
+        if (start && end) {
             const filteredRides = allRides.filter((ride) => {
                 const isClose = (coor1, coor2) => {
                     // .15 is equal to a 10 mile radius around a coordinate
                     return Math.abs(coor1 - coor2) < 0.15;
                 };
-                const startLatDif = isClose(
-                    ride.start_location.lat,
-                    start_location.lat
-                );
+
+                const startLatDif = isClose(ride.start_location.lat, start.lat);
                 const startLongDif = isClose(
                     ride.start_location.long,
-                    start_location.long
+                    start.long
                 );
-                const endLongDif = isClose(
-                    ride.end_location.long,
-                    end_location.long
-                );
-                const endLatDif = isClose(
-                    ride.end_location.lat,
-                    end_location.lat
-                );
+                const endLongDif = isClose(ride.end_location.long, end.long);
+                const endLatDif = isClose(ride.end_location.lat, end.lat);
                 if (startLatDif && startLongDif && endLongDif && endLatDif) {
                     return ride;
                 }
@@ -63,7 +60,25 @@ router.get("/", async (req, res, next) => {
 
             res.json(await mapped);
         } else {
-            res.json(allRides);
+            const mapped = Promise.all(
+                filteredRides.map(async (cur) => {
+                    const hobbies = await hobbiesDB.findByUser(cur.driver_id);
+                    const audio_likes = await audio_likesDB.findByUser(
+                        cur.driver_id
+                    );
+                    const audio_dislikes = await audio_dislikesDB.findByUser(
+                        cur.driver_id
+                    );
+
+                    return {
+                        ...cur,
+                        hobbies,
+                        audio_likes,
+                        audio_dislikes
+                    };
+                })
+            );
+            res.json(mapped);
         }
     } catch (err) {
         next(err);
