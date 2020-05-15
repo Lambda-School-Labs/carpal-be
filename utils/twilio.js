7bd77e2ea03832d951c4bb309b6913024948ba05
smv5047 <smv5047@gmail.com>
2020-05-14 21:21:31 -0400

    started mapbox trip duration feature

--- utils/twilio.js
+++ utils/twilio.js
@@ -1,1 +1,12 @@
 const client = require("../config/twilio");
+const rideETA = require("./eta");
+
+function twilio(rideETA, rider_phone_number) {
+    client.messages.create({
+        body: `Your ride has been confirmed. Your driver will be there in ${rideETA}`,
+        from: process.env.TWILIO_FROM_PHONE,
+        to: `+1${rider_phone_number}`
+    });
+}
+
+module.exports = twilio;
